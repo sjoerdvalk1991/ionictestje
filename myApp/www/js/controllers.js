9761fd383337e7d86634ecc73a4178e73b9020d2
Sjoerd Valk <sjoerd@ivaldi.nl>
2014-11-03 16:20:42 +0100

    add ionic

---
+++ myApp/www/js/controllers.js
@@ -0,0 +1,40 @@
+var app = angular.module('starter.controllers', []);
+
+var appController = function($scope, $ionicModal, $timeout, $location){
+
+  var _this = this;
+  // Form data for the login modal
+  this.loginData = {};
+
+  this.hasMenu = true;
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    _this.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+ this.closeLogin = function() {
+    _this.modal.hide();
+  };
+
+  // Open the login modal
+  this.login = function() {
+    _this.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  this.doLogin = function() {
+    console.log('Doing login', this.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      _this.closeLogin();
+    }, 1000);
+  };
+}
+appController.$inject = ['$scope', '$ionicModal', '$timeout','$location'];
+app.controller('AppCtrl', appController);
---
+++ myApp/www/js/routes.js
@@ -0,0 +1,33 @@
+var app = angular.module('starter', ['ionic', 'starter.controllers']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+      url: "/app",
+      abstract: true,
+      templateUrl: "templates/menu.html",
+      controller: 'AppCtrl'
+    })
+
+    .state('app.search', {
+      url: "/search",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/search.html"
+        }
+      }
+    })
+
+    .state('app.browse', {
+      url: "/browse",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/browse.html"
+        }
+      }
+    })
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/search');
+});
+
